a6d3ca3d5f246ab05fd71ec4e9addc1048cb394e
Elvis <elvisdedic@outlook.com>
2015-06-04 16:23:17 +0200

    Update and rename Intro.js to Intro.txt

--- Javascript/LearningBehaviorDrivenDevelopment/Intro.js
+++
@@ -1,7 +0,0 @@
-As a summary, the following are the five rules of the test-first approach:
-
-•Don't write any new tests if there is not a new coding task.
-•A new test must always fail.
-•A new test should be as simple as possible.
-•Write only the minimum necessary code to fix a failing test, and don't bother with quality during this activity.
-•You can only clean or redesign your code if all the tests pass. Try to do it in each cycle if possible.
