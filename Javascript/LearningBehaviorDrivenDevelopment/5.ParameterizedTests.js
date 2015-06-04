ea8cc93455cd2393df3eb8f207c793519a74f6eb
Elvis <elvisdedic@outlook.com>
2015-06-04 18:49:56 +0200

    Create 5.ParameterizedTests.js

---
+++ Javascript/LearningBehaviorDrivenDevelopment/5.ParameterizedTests.js
@@ -0,0 +1,45 @@
+//The beforeEach function is executed once before each it function inside a given describe scope
+describe('A Validator', function() {
+  var validator;
+  beforeEach(function() {
+    validator = validatorWith([
+      nonPositiveValidationRule,
+      nonDivisibleValidationRule(3, 'error.three'),
+      nonDivisibleValidationRule(5, 'error.five')
+    ]);
+  });
+
+  it('will return no errors for valid numbers', function() {
+    expect(validator(7)).to.be.empty;
+  });
+
+  describe('will include error.nonpositive for not strictly positive numbers:', function() {
+    it('like 0', function() {
+      expect(validator(0)).to.include('error.nonpositive');
+    });
+
+    it('like -2', function() {
+      expect(validator(-2)).to.include('error.nonpositive');
+    });
+  });
+
+  describe('will include error.three for divisible by 3 numbers:', function() {
+    it('like 3', function() {
+      expect(validator(3)).to.include('error.three');
+    });
+
+    it('like 15', function() {
+      expect(validator(15)).to.include('error.three');
+    });
+  });
+
+  describe('will include error.five for divisible by 5 numbers:', function() {
+    it('like 5', function() {
+      expect(validator(5)).to.include('error.five');
+    });
+
+    it('like 15', function() {
+      expect(validator(15)).to.include('error.five');
+    });
+  });
+});
