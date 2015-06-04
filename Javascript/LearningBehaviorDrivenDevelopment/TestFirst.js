e0333b2b5cc522573b37f3ed79498d98421a2631
Elvis <elvisdedic@outlook.com>
2015-06-04 16:52:02 +0200

    Create TestFirst.js

---
+++ Javascript/LearningBehaviorDrivenDevelopment/TestFirst.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+//The describe function creates a new test suite for Mocha. 
+//A test suite is just a grouping of test cases with a nice description
+describe('A Validator', function() {
+
+});
